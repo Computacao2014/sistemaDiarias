54f165b01b282f8684ed45cd39257cadd58f3237
root <root@linux-pc>
2016-12-01 16:53:20 -0300

    Adicionado link de sair da sessão

--- classes/pagina.php
+++ classes/pagina.php
@@ -100,70 +100,17 @@ class Pagina {
                   </ul>
                 </li>
               </ul>
-                <ul class="nav navbar-nav navbar-right" style="border: solid 1px white">
-                    <li><a href="#">Nome</a></li>
-                    <li>Sair</li>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Nome<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="controller/logica_deslogar.php">Sair</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
           </nav>
           
-          <!--Login Modal -->
-          <div class="modal fade" id="myModal" role="dialog">
-            <div class="modal-dialog modal-xg">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  <h4 class="modal-title">Entrar no sistema</h4>
-                </div>
-                <div class="modal-body">
-                    <form>
-                      <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" class="form-control" id="email">
-                      </div>
-                      <div class="form-group">
-                        <label for="senha">Senha:</label>
-                        <input type="password" class="form-control" id="senha">
-                      </div>
-                      <div class="checkbox">
-                          <label><input type="checkbox" class="checkbox" disabled>Lembrar-me</label>
-                      </div>
-                        
-                      <div class="row">
-                          <div class="col-xs-1"></div>
-                          
-                          <button type="submit" class="btn btn-success col-xs-5">Login</button>
-                          
-                          <div class="col-xs-4">
-                              <a href="#">
-                                <button class="btn btn-warning">Esqueci a senha</button>
-                              </a>
-                          </div>
-                          
-                      </div>  
-                      
-                    </form>
-                </div>
-                  
-                <div class="modal-footer row">
-                    <div class="col-xs-3">
-                        <a href="cadastro_servidores.php">
-                          <button class="btn btn-default">Não possuo cadastro</button>
-                        </a>
-                    </div>
-                    
-                    <div class="col-xs-5">
-                        
-                    </div>
-                    
-                    <div class="col-xs-3">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-                    </div>
-                  
-                </div>
-              </div>
-            </div>
-          </div>
         
         
         <?php
